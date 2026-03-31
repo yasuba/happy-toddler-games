c03170fdb218a55cba762e7c112648849e159e36
Your Name <you@example.com>
2026-03-31 08:29:38 +0100

    disable jekyll processing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const BASE = self.location.pathname.replace(/\/sw\.js$/, '');
-const CACHE_NAME = 'happy-toddler-games-v2';
+const CACHE_NAME = 'happy-toddler-games-v3';
 
 const ASSETS = [
   BASE + '/',
